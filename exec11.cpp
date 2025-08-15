d253d6c0e889af005e0f3d2f0f1c4278653ddb41
Pedro Henrique <109990820+drohoug@users.noreply.github.com>
2025-08-15 10:11:57 -0300

    Create exec11.cpp

---
+++ exec11.cpp
@@ -0,0 +1,5 @@
+//* @sobre  Exercicio 01 da aula 0 de programação estruturada
+//* @author  Pedro Henrique RU: 5194697
+
+
+//Crie uma função que declare uma variável estática. A função deve incrementar o valor da //variável estática e imprimir o seu valor. Chame esta função várias vezes no seu programa //principal e observe o comportamento da variável estática.
